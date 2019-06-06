0a0ae3d1044cf1d73b1b294bb4ee2a166a98f165
Dani Sadorf <dani.noelani@gmail.com>
2019-06-06 13:40:06 -0600

    Filter fixed

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,31 +10,36 @@ const filterByParty = (senators, party) => senators.filter(senator => senator.pa
 
 const getSenators = async (url) => fetch(url).then(res => res.json()).then(data => data.objects)
 
-const searchSenators = (senators = [], filter = {}) => senators.filter(({party, state}) => (filter.party && filter.party.length > 0 && party === filter.party) || (filter.state.length > 0 && state === state.toUpperCase()))
+const searchSenators = (senators = [], filter = { party: '', state: '' }) => {
+  // Make sure the filter.party and filter.state are both being used
+  if (filter.party && filter.state)
+    return senators.filter(senator => filter.party === senator.party && filter.state === senator.state.toUpperCase())
+
+  // Determine which filter is being used and filter appropriately predicated upon the filter object 
+  return filter.party ?
+    filterByParty(senators, filter.party) :
+    filterByState(senators, filter.state)
+}
 
 export default () => {
   const [senators, setSenators] = useState([])
-  const [searched, setSearched] = useState(senators)
-  // console.log(senators);
-  // console.log(searched)
+  const [searched, setSearched] = useState([])
 
   useEffect(() => {
-    getSenators(URI).then(results => setSenators(results))
-    }, [senators, searched]);
+    getSenators(URI)
+      .then(results => setSenators(results))
+  }, [senators, searched]);
 
 
   return (
     <div>
       <Form onSearched={filter => {
-        let filtered = searchSenators(senators, filter)
-        // console.log(filtered)
-        // filtered = filter.party && filterByParty(filtered, filter.party) || []
+        const filtered = searchSenators(senators, filter)
         setSearched(filtered)
-        // console.log(filtered)
       }} />
       <ul>
-        {searched.map(senator => (
-          <li>{senator.person.lastname}</li>
+        {searched.map((senator, index) => (
+          <li key={index}>{senator.person.lastname} | {senator.party}</li>
         ))}
       </ul>
     </div>
